b6e99fc00f36e2066d9b136c87abe07cc6ed8cbd
Oskar Gewalli <oskar@gewalli.se>
2017-12-13 23:22:09 +0100

    No setters for bid

--- src/Domain/Bids.hs
+++ src/Domain/Bids.hs
@@ -5,10 +5,19 @@ import Domain.Prelude
 import GHC.Generics
 import Data.Time
 
-data Bid = Bid { bidId :: BidId,
-  forAuction :: AuctionId,
-  bidder :: UserId,
-  at :: UTCTime,
-  bidAmount :: Amount
-} deriving (Generic, Show)
+data Bid = Bid BidId AuctionId UserId UTCTime Amount deriving (Generic, Show)
 
+bidId :: Bid -> BidId
+bidId (Bid bidId _ _ _ _) = bidId
+
+forAuction :: Bid -> AuctionId
+forAuction (Bid _ auction _ _ _) = auction
+
+bidder :: Bid -> UserId
+bidder (Bid _ _ user _ _) = user
+
+at :: Bid -> UTCTime
+at (Bid _ _ _ time _) = time
+
+bidAmount :: Bid -> Amount
+bidAmount (Bid _ _ _ _ amount) = amount
